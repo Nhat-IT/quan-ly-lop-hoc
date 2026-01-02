1a3e57b1ed6abad6bf3b3525847ec313e86a5b79
Nhat-IT <22050004@student.bdu.edu.vn>
2026-01-02 12:37:39 +0700

    Sua duong dan API

--- public/script.js
+++ public/script.js
@@ -52,7 +52,7 @@ class LoginApp {
         
         try {
             // GỌI API BACKEND
-            const response = await fetch('http://localhost:3000/api/auth/login', {
+            const response = await fetch('/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
